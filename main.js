50f88aa5826395a3c9a6299aa72cbcf50590a11d
alex-look-987 <sqrpshr.23@gmail.com>
2025-12-13 13:22:00 -0600

    feat: Virtual Doom

--- main.js
+++ main.js
@@ -1,56 +1,20 @@
 const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement);
 
-/* 
-const element = React.createElement(
-    type = 'div',
-    props = {
-        className: 'container',
-    },
-    children = ['Hello World', 'Goodbye To A World']
-);
+function renderElement () {
+  const element = (
+    <div className="post">
+      <h1>My First Blog Post</h1>
+      <div>Author: Mark Twain</div>
+      <div>Published: {new Date().toLocaleTimeString()}</div>
+      <p>
+        I am new to blogging and this is my first post. You should expect a
+        lot of great things from me.
+      </p>
+    </div>
+  );
 
-console.log(element); */
+  root.render(element);
+}
 
-/* const person = {first: 'Joe', last: 'Doe'};
-const element =  <div className="container">
-    Hello {person.first} {person.last}
-     </div>; */
-
-
-const test = (
-  <ul>
-    <li>
-      <a href="">a</a>
-    </li>
-    <li>
-      <a href="">b</a>
-    </li>
-    <li>
-      <a href="">c</a>
-    </li>
-  </ul>
-);
-
-const logo = {
-  name: 'React Logo',
-  title: 'React Logo',
-  path: './react-logo.png',
-};
-
-const element = <img src={logo.path} alt={logo.name} title={logo.title} />;
-
-ReactDOM.createRoot(rootElement).render(element);
-
-/* 
-The method signature for createElement is as follows:
-
-React.createElement(type, [props], [...children])
-
-
-
-const element = document.createElement('div');
-
-element.textContent = 'Hello World';
-element.className = 'container';
-
-rootElement.appendChild(element); */
+setInterval(renderElement, 1000);
