30155ee2afd99744085636b1bcd326986c1d4e85
alex-look-987 <sqrpshr.23@gmail.com>
2025-12-18 10:15:36 -0600

    feat: Props

--- projectpilot/src/main.tsx
+++ projectpilot/src/main.tsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { StrictMode } from 'react'
+import Greeter from './props/Greeter.tsx'
+import { createRoot } from 'react-dom/client'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Greeter first="Srini" last='Kata' />
     <App />
   </StrictMode>,
 )
---
+++ projectpilot/src/props/Greeter.tsx
@@ -0,0 +1,10 @@
+function Greeter({ first, last }: any) {
+  // const { first, last } = props;
+  return (
+    <h1>
+      Hello, {first} {last}
+    </h1>
+  );
+}
+
+export default Greeter
