15c514427377fabda642c508c6ba9716e0660832
alex-look-987 <sqrpshr.23@gmail.com>
2025-12-18 17:51:18 -0600

    feat: React & Typescript

--- projectpilot/src/App.tsx
+++ projectpilot/src/App.tsx
@@ -1,11 +1,29 @@
-import ProjectsPage from './projects/ProjectsPage';
+import './App.css';
+import React from "react";
+import Hello from './Hello';
+
+const App: React.FC = () => {
+  return (
+      <div className='App'> 
+      <Hello name='Elvis' enthusiasmLevel={3}> 
+      </Hello> 
+      </div> 
+  );
+};
+
+export default App
+
+/* import ProjectsPage from './projects/ProjectsPage';
+import Hello from './Hello';
 
 function App() {
   return (
     <div className='container'>
+      <Hello name='David'/>
       <ProjectsPage/>
     </div>
   );
 }
 
 export default App;
+ */
---
+++ projectpilot/src/Hello.tsx
@@ -0,0 +1,57 @@
+import React, {SyntheticEvent} from "react";
+
+export interface Props {
+    name: string;
+    enthusiasmLevel?: number;
+}
+
+interface State {
+    currentEnthusiasm: number
+}
+
+// helpers
+
+function getExclamationMarks(numChars: number) {
+  return Array(numChars + 1).join("!");
+}
+
+class Hello extends React.Component<Props, State> {
+    state = {currentEnthusiasm: this.props.enthusiasmLevel || 1}
+
+    updateEnthusiasm(change: number) {
+        this.setState((currentState) => {
+            return {currentEnthusiasm: currentState.currentEnthusiasm + change};
+        });
+    } 
+
+    onIncrement = (event: SyntheticEvent) => {
+        console.log(event)
+        this.updateEnthusiasm(1);
+    }
+
+    onDecrement = (event: SyntheticEvent) => {
+        console.log(event.target)
+        this.updateEnthusiasm(-1);
+    }
+    
+    render() {
+        const { name} = this.props;
+
+        if (this.state.currentEnthusiasm <= 0) {
+            throw new Error("You could be a little more enthusiastic. :D");
+        }
+
+        return (
+        <div className="hello">
+            <div className="greeting">
+            Hello {name + getExclamationMarks(this.state.currentEnthusiasm)}
+            </div>
+            <button onClick={this.onIncrement}></button>
+            <button onClick={this.onDecrement}></button>
+        </div>
+        );
+    }
+}
+
+export default Hello;
+
