a2ada9b865f7ff7e372d5bdddb3dde243e0f6dc9
alex-look-987 <sqrpshr.23@gmail.com>
2025-12-23 23:26:09 -0600

    feat: Hello Button explanation

--- projectpilot/src/Hello.tsx
+++ projectpilot/src/Hello.tsx
@@ -9,40 +9,43 @@ interface State {
     currentEnthusiasm: number
 }
 
-// helpers
-
-function getExclamationMarks(numChars: number) {
-  return Array(numChars + 1).join("!");
-}
-
 class Hello extends React.Component<Props, State> {
+    // state its an object with a currentEnthusiasm property (see State interface)
+    // if Hello component starts empty, default value as 1
     state = {currentEnthusiasm: this.props.enthusiasmLevel || 1}
 
     updateEnthusiasm(change: number) {
+        // setState takes current value inside function through currenState argument
+        // same resource/behaviour as state in the return of the function inside setState
          this.setState((currentState) => ({
-            currentEnthusiasm: 
+            currentEnthusiasm:
             Math.max(0, currentState.currentEnthusiasm + change)
         }));
     } 
 
     onIncrement = (event: SyntheticEvent) => {
-        console.log(event)
+        console.log(event.currentTarget)
         this.updateEnthusiasm(1);
     }
 
     onDecrement = (event: SyntheticEvent) => {
-        console.log(event.target)
+        console.log(event.currentTarget)
         this.updateEnthusiasm(-1);
+    }
+
+    getExclamationMarks(numChars: number) {
+        return Array(numChars + 1).join("!");
     }
     
     render() {
-        const {name} = this.props;
+        // property from props. takes name property and declare variable with same name
+        const {name} = this.props; 
 
         return (
         <div className="hello">
             <div className="greeting">
             Hello {name}
-            {this.state.currentEnthusiasm > 0 && getExclamationMarks(this.state.currentEnthusiasm)}
+            {this.state.currentEnthusiasm > 0 && this.getExclamationMarks(this.state.currentEnthusiasm)}
             </div>
             <button onClick={this.onIncrement}>Increment</button>
             <button onClick={this.onDecrement} disabled = {this.state.currentEnthusiasm === 0}>Decrement</button>
--- projectpilot/src/projects/ProjectsPage.tsx
+++ projectpilot/src/projects/ProjectsPage.tsx
@@ -1,12 +1,10 @@
 import { MOCK_PROJECTS } from "./MockProjects"
 
 function ProjectsPage () {
-    return (
-        
+    return (        
         <>
         <h1>Projects</h1>
         <pre>{JSON.stringify(MOCK_PROJECTS, null, ' ')}</pre>
-
         </>
     )
 
